9dd753aa1d5c3af44012b262eadd633f614632b5
bagdaddanslevan <83098861+bagdaddanslevan@users.noreply.github.com>
2021-11-04 16:51:21 +0100

    f2
    maj full ok

--- Accueil/post_contact.php
+++ Accueil/post_contact.php
@@ -1,20 +1,46 @@
 <?php
 
-if(isset($_POST['submit'])){
-      $name = $_POST['prenom'];
-      $lastName = $_POST['nom'];
-      $mailForm = $_POST['email'] ;
-      $message = '<b>date_entree : </b>' . $_POST['date_entree'] . '</p>'.'<br>
-<b>date_sortie : </b>' . $_POST['date_sortie'] . '</p>'.'<br>
-<b>nb_adultes : </b>' . $_POST['nb_adultes'] . '</p>'.'<br>
-<b>nb_enfants : </b>' . $_POST['nb_enfants'] . '</p>'.'<br>
-<b>logement : </b>' . $_POST['logement'] . '</p>'.'<br>
-<b>options : </b>' . $_POST['options'] . '</p>';
+session_start();
 
-$mailTo ="chimenbou.angelou@gmail.com";
-$headers = "From: " . $mailFrom;
-$txt = "you have received an e-mail from ".$name.".\n\n".$message;
+$name = $_POST['prenom'];
+$lastName = $_POST['nom'];
+$email = $_POST['email'];
+$arrivee = $_POST['arrivee'];
+$date_sortie = $_POST['sortie'];
+$logement = $_POST['logement'];
+$nb_adultes = $_POST['nb_adultes'];
+$nb_enfants_13 = $_POST['nb_enfants_13'];
+$nb_enfants_2 = $_POST['nb_enfants_2'];
 
-    mail($mailTo,$txt,$headers);
-}
-?>
+
+$champagne = $_POST['champagne'];
+$Vins = $_POST['vins'];
+$petales = $_POST['petales'];
+$mix = $_POST['mixte'];
+$vegetarienne = $_POST['vegetarienne'];
+$vegetalienne = $_POST['vegetalienne'];
+$fondue = $_POST['fondue'];
+$msg = $_POST['text'];
+
+$message =  $nom . ' ' . $prenom . ' ' . ' aimerait le logement ' . $logement . ' pour  ' .
+    ".\n" .
+    $nb_adultes . ' adulte(s) ' .
+    ".\n" .
+    $nb_enfants_13 . ' enfant(s) de moins de 13 ans '
+    . ".\n" .
+    $nb_enfants_2 . ' enfant(s) de moins de 2 ans ' .
+    ".\n" .
+    ' et en options ' .
+    ".\n" .
+    $champagne . ' ' . $Vins . ' ' . $petales .
+    ' ' . $mix . ' ' . $vegetarienne . ' ' . $vegetalienne . ' ' . $fondue
+    . ".\n" .
+    'a patir du ' . $arrivee . ' jusqu\'au ' . $date_sortie
+    . ".\n" . ".\n" .
+    $msg . ' ' . $email;
+
+
+$headers = "Reservation : " . $email . '';
+$txt = "Vous avait recu un e-mail de " . $name . ' ' .  $lastName .  ".\n\n" . $message;
+mail("lorie.molina134@gmail.com", $headers, $txt);
+header('Location: index.html');
